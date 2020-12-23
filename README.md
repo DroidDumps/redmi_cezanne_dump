5ead2b6e850fb8f2df52e9da2ddd4897bd3327e3
DroidDumps <rokibhasansagar2014@outlook.com>
2020-12-23 15:16:09 +0000

    Add extras for cezanne-user 11 RP1A.200720.011 20.12.16 release-keys
    Signed-off-by: DroidDumps <rokibhasansagar2014@outlook.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+## cezanne-user 11 RP1A.200720.011 20.12.16 release-keys
+- Manufacturer: xiaomi
+- Platform: mt6885
+- Codename: cezanne
+- Brand: Redmi
+- Flavor: cezanne-user
+- Release Version: 11
+- Id: RP1A.200720.011
+- Incremental: 20.12.16
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- Locale: zh-CN
+- Screen Density: 440
+- Fingerprint: Redmi/cezanne/cezanne:11/RP1A.200720.011/20.12.16:user/release-keys
+- Branch: cezanne-user-11-RP1A.200720.011-20.12.16-release-keys
+- Repo: redmi_cezanne_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> vendor/data-app/SmartHome/SmartHome.apk
+rm -f vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat vendor/camera/mimoji/model2.zip.* 2>/dev/null >> vendor/camera/mimoji/model2.zip
+rm -f vendor/camera/mimoji/model2.zip.* 2>/dev/null
+cat system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null >> system/system/data-app/Youpin/Youpin.apk
+rm -f system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null
+cat system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null >> system/system/data-app/MiShop/MiShop.apk
+rm -f system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null
+cat system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk.* 2>/dev/null >> system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk
+rm -f system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk.* 2>/dev/null
+cat system/system/data-app/com.smile.gifmaker_13/com.smile.gifmaker_13.apk.* 2>/dev/null >> system/system/data-app/com.smile.gifmaker_13/com.smile.gifmaker_13.apk
+rm -f system/system/data-app/com.smile.gifmaker_13/com.smile.gifmaker_13.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null >> system/system/priv-app/MiuiVideo/MiuiVideo.apk
+rm -f system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
